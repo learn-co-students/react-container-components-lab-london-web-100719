e11785987401654dc94f411a3b61e6602e62e912
Tish Chungoora <tishal82@gmail.com>
2019-12-15 19:37:01 +0000

    Implemented the ability to view relevant movie reviews info

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+import React, { Component } from "react";
+import "isomorphic-fetch";
+import MovieReviews from "./MovieReviews";
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = "TZAkRaf2GL2l7YsGOAwtZV2TyQ7EFtJu";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/search.json?" +
+  `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        Placeholder for latest movie reviews
+      </div>
+    );
+  }
+}
